181353ed29bc24ef8f000065af121f95bc20c316
Arne Diekmann <arne@neoskop.de>
2024-07-10 10:27:00 +0200

    chore: release 0.0.3

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
-npm version --no-git-tag-version $1
+version=$(npm version --no-git-tag-version $1)
 npm run gv
 npm run build
 npm publish
 git add -A
-git commit -m "Release $1"
-git tag $1
+git commit -m "chore: release $version"
+git tag $version
 git push
 git push --tags
